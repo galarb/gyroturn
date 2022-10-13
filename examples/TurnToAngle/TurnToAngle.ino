d623fc5947a278a4ffc2e8cfe5a3778b1d12529b
galarb <84223483+galarb@users.noreply.github.com>
2022-10-13 19:37:39 +0300

    control of Ki Kp Kd

--- examples/TurnToAngle/TurnToAngle.ino
+++ examples/TurnToAngle/TurnToAngle.ino
@@ -1,16 +1,18 @@
 
 
 #include "gyroturn.h"
-gyroturn myturn(2, 4, 7, 8, 3, 5);  //in1, in2, in3, in4, enA, enB in L298N. 
+gyroturn myturn(2, 4, 7, 8, 3, 6);  //in1, in2, in3, in4, enA, enB in L298N. 
 
 
 void setup() {
-myturn.begin(); //starts serial monitor at 115200
+myturn.begin(3, 0.007, 1); //Kp, Ki, Kd
+
 }
 
 void loop() { 
   
- myturn.gotoang(90);
+ myturn.gotoang(-90);
   delay(100);
+ 
    
  }
